c50041ca7f7ee85e5b386d3337038876cff3ca16
ravidat <36666816+ravidat@users.noreply.github.com>
2018-11-05 20:35:54 +0200

    add some char parser functionality

--- Assignment1.ml
+++ Assignment1.ml
@@ -2,10 +2,14 @@
 #use "reader.ml";;
 open PC;;
 
+(*service function *)
+
 let spaces = star (char ' ');;
 
 let hashtagParser = char ('#');;
+(* end service function *)
 
+(*boolean function *)
 let trueParser =
   let _t_ = char_ci 't' in
   let _hashT_ = caten hashtagParser _t_ in
@@ -17,51 +21,57 @@ let falseParser =
   pack _hashF_ (fun x -> false);;
 
 let booleanParser = disj falseParser trueParser;; 
+(* end boolean function *)
 
-test_string booleanParser "#t";;
 
+(* char parser *)
 let charPrefixParser =
   let backslashParser = char '\\' in
-  caten hashtagParser backslashParser;; 
+  let _charPrefixParser =   caten hashtagParser backslashParser in
+  pack _charPrefixParser (fun x  -> '#');;
 
-let visibleSimpleCharPrefix = const (fun ch -> ch > ' ');;
+let visibleSimpleCharPrefix =
+  let simpleCharParser = const (fun ch -> ch > ' ') in
+  pack simpleCharParser (fun x -> x);;
 
-(*
-let charParser = disj (caten charPrefixParser visibleSimpleCharPrefix) namedCharParser;;
-
-let namedCharParser = disj
-  newLineParser
-  returnParser
-  tabParser
-  pageParser
-  nulParser
-  spaceParser;;
-*)
 
 let newLineParser =
   let _newLineParser = word_ci "newline" in
-  pack _newLineParser (fun nch -> '\n');;
+  pack _newLineParser (fun nch -> Char.chr(10));;
 
-let returnParser=
+let returnParser =
   let _returnParser  = word_ci "return" in
-  pack _returnParser (fun nch -> '\r');;
-  
+  pack _returnParser (fun nch -> Char.chr(13));;
+ 
 let tabParser =
   let _tabParser = word_ci "tab" in
-  pack _tabParser (fun nch -> '\t');;
+  pack _tabParser (fun nch -> Char.chr(9));;
 
 let pageParser =
   let _pageParser = word_ci "page" in
-  pack _pageParser (fun nch -> '\p');;
+  pack _pageParser (fun nch -> Char.chr(12));;
 
 let nulParser =
   let _nulParser = word_ci "nul" in
-  pack _nulParser (fun nch -> '\0');;
+  pack _nulParser (fun nch -> Char.chr(0));;
 
 let spaceParser =
   let _spaceParser = word_ci "space" in
-  pack _spaceParser (fun nch -> '\0');;
+  pack _spaceParser (fun nch -> Char.chr(32));;
+
+let namedCharParser = disj_list [returnParser; newLineParser; tabParser; pageParser; nulParser; spaceParser];;
+
+let charParser =
+  let charAsList =  disj_list[caten charPrefixParser namedCharParser ;
+                              caten charPrefixParser visibleSimpleCharPrefix ] in
+  pack charAsList (fun l -> snd l);;
+(* end char parser *)
+let xHexChar =
+  let _x_ = char_ci 'x' in
+    pack _x_ (fun x -> Scan.sscanf x "%x%!" fun(x -> x));;
 
 
-test_string newLineParser "newline";; 
+
+test_string xHexChar "xa";; 
+
 
